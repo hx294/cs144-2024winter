25acc02492525487e6a47aa8fec0cbecd49c67e3
hx294 <2819915006@qq.com>
2024-12-10 21:55:15 +0800

    byte_stream : writer has done

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -7,59 +7,72 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return closed;
 }
 
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
-  return;
+  for(auto &c: data)
+    if(mem.size() <= capacity_){
+      mem.push_back(c);
+      pushed_cnt++;
+    }else {
+      break;
+    }
 }
 
 void Writer::close()
 {
   // Your code here.
+  closed = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return capacity_- mem.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return pushed_cnt;
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return closed && mem.size()==0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return poped_cnt;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  return string_view(mem.begin(),mem.end());
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  while(len--) {
+    if(mem.size() > 0){
+      poped_cnt ++;
+      mem.pop_front();
+    }else{
+      break;
+    }
+  }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return mem.size();
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <deque>
 
 class Reader;
 class Writer;
@@ -25,6 +26,10 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+  bool closed = false;
+  deque<char> mem;
+  uint64_t pushed_cnt;
+  uint64_t poped_cnt;
 };
 
 class Writer : public ByteStream
