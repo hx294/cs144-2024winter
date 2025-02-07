652832b3088b54ba115233607c595c83958b309f
hx294 <2819915006@qq.com>
2025-02-07 23:55:28 +0800

    ues assign method to copy from string_view to string

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,6 +1,7 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -17,11 +18,18 @@ uint64_t TCPSender::consecutive_retransmissions() const
 void TCPSender::push( const TransmitFunction& transmit )
 {
   TCPSenderMessage m;
+
+  // 上次的发送任务还未完成
+  if(last_out_index_next_ != first_out_index_) {
+    return;
+  }
+
   // 窗口大小为0
   if( window_size_ == 0 ) {
     // 伪装成窗口为1的样子
     m = {isn_,true,"",false,false};
     transmit(m);
+    last_out_index_next_ ++;
   } else {
     Reader& r = input_.reader();
     string_view s = r.peek();
@@ -30,7 +38,7 @@ void TCPSender::push( const TransmitFunction& transmit )
     first_out_index_ = last_out_index_next_;
   
     // 第一个并携带SYN标志的报文段
-    if( r.bytes_popped() == 0 && !r.is_finished() ) {
+    if( first_out_index_ == isn_.unwrap(isn_,0) && !r.is_finished() ) {
       // 实际的序列号为 要包括SYN
       left_space --;
       m.SYN = true;
@@ -42,7 +50,7 @@ void TCPSender::push( const TransmitFunction& transmit )
       // 一个报文段的大小是剩余窗口大小，报文段最大值，外出字节流未pop量的最小值。
       uint64_t msg_size = TCPConfig::MAX_PAYLOAD_SIZE - m.sequence_length();
       left_space -= msg_size;
-      s.copy(m.payload.data(),msg_size);
+      m.payload.assign(s.data(), msg_size);
       m.seqno = Wrap32::wrap(last_out_index_next_,isn_);
       outstanding_segments_.push(move(m));
       transmit(outstanding_segments_.back());
@@ -53,31 +61,38 @@ void TCPSender::push( const TransmitFunction& transmit )
 
     // 发送最后一个报文段，不是最大报文段
     uint64_t last_seg_size = min( left_space, s.size() );
-    left_space -= last_seg_size;
-    s.copy(m.payload.data(),last_seg_size);
-    s = s.substr(last_seg_size, s.size() - last_seg_size);
-    // 是否需要承载一个FIN,以及窗口是否能容纳
-    if( r.is_finished() && left_space) {
-        m.FIN = true;
-        left_space --;
+    if(last_seg_size > 0) {
+      left_space -= last_seg_size;
+      m.payload.assign(s.data(),last_seg_size);
+      cout << "payload: " << m.payload << endl;
+      m.seqno = Wrap32::wrap(last_out_index_next_,isn_);
+      cout << "s: " << s << endl;
+      s = s.substr(last_seg_size, s.size() - last_seg_size);
+      // 是否需要承载一个FIN,以及窗口是否能容纳
+      if( r.is_finished() && left_space) {
+          m.FIN = true;
+          left_space --;
+      }
+      outstanding_segments_.push(move(m));
+      transmit(outstanding_segments_.back());
+      last_out_index_next_ += outstanding_segments_.back().sequence_length();
+      r.pop(last_out_index_next_-first_out_index_+outstanding_segments_.front().SYN + outstanding_segments_.back().FIN);
     }
-    outstanding_segments_.push(move(m));
-    transmit(outstanding_segments_.back());
-    last_out_index_next_ += outstanding_segments_.back().sequence_length();
-
-    r.pop(last_out_index_next_ - first_out_index_);
 
   }
-  consecutive_++;
 
-  if(timer_.has_started() == false) {
-    timer_.start();
+  if (outstanding_segments_.size() ) {
+    consecutive_++;
+
+    if(timer_.has_started() == false) {
+      timer_.start();
+    }
   }
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
-  return {Wrap32::wrap(first_out_index_,isn_),false,"",false,false};
+  return {Wrap32::wrap(last_out_index_next_,isn_),false,"",false,false};
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
@@ -93,7 +108,7 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
   window_size_ = msg.window_size;
 
   // 所有未完成的数据被接收后，停止重发计时器
-  if(left_ + window_size_ == Wrap32::wrap(last_out_index_next_,isn_)) {
+  if(left_ == Wrap32::wrap(last_out_index_next_,isn_)) {
     timer_.stop();
   }
 
@@ -103,6 +118,8 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
     first_out_index_ += outstanding_segments_.front().sequence_length();
     outstanding_segments_.pop();
   }
+
+  first_out_index_ = left_.unwrap(isn_,first_out_index_);
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
